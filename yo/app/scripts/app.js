99243a427cf7d8f3f213979ec1df1e9e45b9a6f7
Olivier Barais <barais@irisa.fr>
2013-09-05 11:55:30 +0200

    initial commit

---
+++ yo/app/scripts/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('yoApp', ['ngResource'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+/*angular.module('yoApp', ['ui.calendar']).config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });*/
+
